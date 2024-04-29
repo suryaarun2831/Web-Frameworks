8a7d5a4214f5cd72738d57e60f82525389dc7743
Surya Arunesh <124980732+suryaarun2831@users.noreply.github.com>
2024-04-29 14:54:07 +0530

    Delete q_04 directory

--- q_04/src/main/java/com/priyanshu/q_04/repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.priyanshu.q_04.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.priyanshu.q_04.model.Patient;
-
-public interface PatientRepo extends JpaRepository<Patient, Integer> {
-
-}
