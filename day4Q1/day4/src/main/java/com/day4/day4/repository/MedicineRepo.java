eca295d90426509971c51205c762eefe9906f58c
Surya Arunesh <124980732+suryaarun2831@users.noreply.github.com>
2024-04-29 14:57:15 +0530

    Delete day4Q1/day4 directory

--- day4Q1/day4/src/main/java/com/day4/day4/repository/MedicineRepo.java
+++
@@ -1,9 +0,0 @@
-package com.day4.day4.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.day4.day4.model.Medicine;
-
-public interface MedicineRepo extends JpaRepository<Medicine,Integer>{
-
-}
